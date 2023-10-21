657d94ace259dc304ffe980488059a7bba298619
Jigyasha Rani <71997541+jigyasha1234@users.noreply.github.com>
2023-10-21 17:41:02 +0530

    adding views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 // module.exports.actionName=function(req,res){}
 
 module.exports.home = function(req,res){
-    return res.end('<h1> home controller is up and running</h1>');
+    return res.render('home', {
+        'title': "Home"
+    })
 }
