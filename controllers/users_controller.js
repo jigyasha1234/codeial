a34dd07d4027b33a1c3136fd6be1eecc264a3539
Jigyasha Rani <71997541+jigyasha1234@users.noreply.github.com>
2023-10-22 20:36:08 +0530

    adding sign in and sign up views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,3 +5,26 @@ module.exports.profile = function(req,res){
         'title': "Users profile"
     })
 }
+//render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+        'title': "codeial || signUp"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+        'title': "codeial || signIn"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req,res){
+    //TODO later
+}
+
+// get the sign in data and create session
+module.exports.createSession = function(req,res){
+    //TODO later
+}
