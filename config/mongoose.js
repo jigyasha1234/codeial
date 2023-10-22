eee1e43ee19504015a71226a1d81a9df50128fb6
Jigyasha Rani <71997541+jigyasha1234@users.noreply.github.com>
2023-10-22 16:37:22 +0530

    connecting mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,'error connecting to mongoDB'));
+db.once('open',function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
